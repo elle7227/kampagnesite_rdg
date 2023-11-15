3119c9be8e7ac3b1e379215fd8eaea4f7d4e12ef
ellen_hein <elle7227@stud.kea.dk>
2023-11-15 13:58:07 -0500

    landing page dag-1

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/Landing.module.css";
+
+export default function Overskrift(){
+    return(
+     
+        <h2 className={styles.heading}>OVERSKRIFT</h2>
+ 
+    
+    )
+}
+
