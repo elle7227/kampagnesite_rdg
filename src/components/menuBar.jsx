0bca321bd550212f8fc530129196807cf71a75ef
ellen_hein <elle7227@stud.kea.dk>
2023-12-13 15:46:50 -0500

    til lighthouse til

--- src/components/menuBar.jsx
+++ src/components/menuBar.jsx
@@ -5,12 +5,12 @@ export default function MenuBar() {
     return (
       <div className={styles.menu_container}>
         <ul className={styles.menu}>
-          <li><a href="/">Home</a></li>
+          <li><a href="/">Events</a></li>
           <li><a href="/recipes">Opskrifter</a></li>
           <li><a href="/inspiration">Inspiration</a></li>
         </ul>
         <div>
-          <img className={styles.menu_logo} src="tidligere_events/small_logo.png"/>  
+          
         </div>
       </div>
     
